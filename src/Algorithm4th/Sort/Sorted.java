96e92d08fe1b9ff66f52a438792e4a26c7f00784
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2020-08-20 23:06:55 +0800

    shell and insert;

--- src/Algorithm4th/Sort/Sorted.java
+++ src/Algorithm4th/Sort/Sorted.java
@@ -3,7 +3,9 @@ package Algorithm4th.Sort;
 public class Sorted {
     public static void main(String[] args) {
         Integer[] arr = {12,4,33,15,7};
-        select(arr);
+//        select(arr);
+//        insert(arr);
+        shell(arr);
         show(arr);
     }
     private static boolean less(Comparable v, Comparable w){
@@ -32,6 +34,25 @@ public class Sorted {
         }
     }
     public static void insert(Comparable[] a){
-        
+        int N = a.length;
+        for(int i=1;i<N;i++){
+            for(int j=i;j>0&&less(a[j],a[j-1]);j--){
+                exec(a,j,j-1);
+                show(a);
+            }
+        }
+    }
+    public static void shell(Comparable[] a){
+        int N =a.length;
+        int h = 1;
+        while(h<N/3) h = h*3 + 1;
+        while(h>=1){
+            for(int i=h;i<N;i++){
+                for(int j=i;j>0&&less(a[j],a[j-h]);j-=h){
+                    exec(a,j,j-h);
+                }
+            }
+            h = h/3;
+        }
     }
 }
