cda3665361166b7653c32da4da3adb1a82d5a1fb
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2020-09-04 18:17:49 +0800

    quick3way

--- src/Algorithm4th/Sort/Quick.java
+++ src/Algorithm4th/Sort/Quick.java
@@ -1,10 +1,9 @@
 package Algorithm4th.Sort;
 import Utils.StdRandom;
 import java.util.Arrays;
-import static Algorithm4th.Sort.Sorted.exec;
-import static Algorithm4th.Sort.Sorted.less;
 
-public class Quick {
+
+public class Quick extends Common{
     public void sort(Comparable[] a){
         //乱序，随机交换元素，消除对输入的依赖
         StdRandom.shuffle(a);
@@ -12,13 +11,13 @@ public class Quick {
     }
     public void sort(Comparable[] a,int lo,int hi){
         //小数组采用插入排序
-        if(lo>=hi) return;
 //        int M = 6;
 //        if(hi<=lo+M){
 //            Insert I = new Insert();
 //            I.insert(Arrays.copyOfRange(a,lo,hi));
 //            return;
 //        }
+        if(lo>=hi) return;
         int i = partition(a,lo,hi);
         //递归
         sort(a,lo,i-1);
@@ -40,5 +39,23 @@ public class Quick {
         }
         exec(a,lo,j);
         return j;
+    }
+    public void quick3way(Comparable[] a,int lo,int hi){
+        if(lo>=hi) return;
+        int lt = lo,i = lo+1,gt=hi;
+        Comparable v = a[lo];
+        while(i<=gt){
+            int cmp = v.compareTo(a[i]);
+            if(cmp>0){
+                //小于切分元素
+                exec(a,i++,lt++);
+            }
+            if(cmp<0){
+                exec(a,i,gt--);
+            }
+            else i++;
+        }
+        quick3way(a,lo,lt-1);
+        quick3way(a,gt+1,hi);
     }
 }
