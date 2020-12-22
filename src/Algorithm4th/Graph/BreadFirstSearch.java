8b7064c8ae2225802e7a2e257d9484874d67f9a3
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2020-12-22 22:14:17 +0800

    图 广度优先 深度优先

---
+++ src/Algorithm4th/Graph/BreadFirstSearch.java
@@ -0,0 +1,48 @@
+package Algorithm4th.Graph;
+
+import java.util.LinkedList;
+
+public class BreadFirstSearch {
+    private boolean[] marked;
+    private int count;
+    private LinkedList<Integer> waitSearch;
+
+    public BreadFirstSearch(Graph G, int s) {
+        this.marked = new boolean[G.V()];
+        this.count = 0;
+        bfs(G,s);
+    }
+
+    private void bfs(Graph G,int v){
+        marked[v] = true;
+        waitSearch.offer(v);
+        while (!waitSearch.isEmpty()){
+            Integer d = waitSearch.pop();
+            for (Integer w : G.adj(v)) {
+                waitSearch.offer(w);
+                marked[w] = true;
+                count++;
+            }
+        }
+
+        //递归为深度优先
+        //bfs(G,waitSearch.pop());
+    }
+
+    /**
+     * w与s是否相连
+     * @param w
+     * @return
+     */
+    public boolean marked(int w){
+        return marked[w];
+    }
+
+    /**
+     * 与顶点相通的数量,包含自身
+     * @return
+     */
+    public int count(){
+        return count;
+    }
+}
