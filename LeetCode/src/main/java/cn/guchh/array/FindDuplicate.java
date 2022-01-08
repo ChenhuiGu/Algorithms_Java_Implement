e7fae94e6145f261978a7b62f36309d8f3254b9b
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2022-01-08 15:56:28 +0800

    二分法发现数组内重复值

--- LeetCode/src/main/java/cn/guchh/array/FindDuplicate.java
+++ LeetCode/src/main/java/cn/guchh/array/FindDuplicate.java
@@ -3,6 +3,7 @@ package cn.guchh.array;
 /**
  * 数组内存在重复数字[1，n]
  * length:n+1
+ * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
  * @author chenhuigu
  */
 public class FindDuplicate {
@@ -25,4 +26,25 @@ public class FindDuplicate {
         }
         return fast;
     }
+
+    public int findDuplicate2(int[] nums) {
+        int len = nums.length;
+        int right = 1,left = len - 1;
+        while (right < left){
+            int cnt = 0;
+            int mid = right + ((left - right)>>1);
+            for(int num:nums){
+                if(num <= mid){
+                    cnt ++;
+                }
+            }
+            if(cnt > mid){
+                left = mid;
+            }else {
+                right = mid+1;
+            }
+        }
+        return left;
+    }
+
 }
